4166164f9c527009387f9330c97d41350a6d1d0d
Max Fierro <maxfierro@berkeley.edu>
2025-05-04 19:19:41 -0700

    Do not track debug.py

--- src/project/debug.py
+++
@@ -1,9 +0,0 @@
-# Script for debugging the gen-activations
-
-from activations import generate_model_activations
-from models.mlp import MLPClassifier
-
-if __name__ == "__main__":
-    game = "mnk_3_3_3"
-    model = MLPClassifier.name()
-    generate_model_activations(game, model)
